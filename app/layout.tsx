9b1ce346055477207ec2745beb5124fd2bef1ef1
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-11 12:54:26 -0500

    Add icon.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,24 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "",
-  description: "",
+    title: "",
+    description: "",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
-  return (
-    <html
-      lang="en"
-    >
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html
+            lang="en"
+        >
+            <head>
+                <link
+                    rel="icon"
+                    href="/icon?<generated>"
+                    type="image/<generated>"
+                    sizes="<generated>"
+                />
+            </head>
+            <body>{children}</body>
+        </html>
+    );
 }
