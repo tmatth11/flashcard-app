438d0a9a75642b2befe22a0f454dc0117728e9d8
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-11 14:28:56 -0500

    Add extra dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <h1>Hello</h1>
-  );
+    return (
+        <header>
+            <nav className="sticky inset-s-0 top-0 w-full border-b bg-neutral-200 p-2">
+                <Link href="/" className="text-2xl font-semibold">
+                    Flashcard App
+                </Link>
+            </nav>
+        </header>
+    );
 }
