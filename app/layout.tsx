be9cc5946a4966a97565b1463e1238d0576c2d81
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-11 15:05:41 -0500

    Add dark mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,23 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import {Inter} from 'next/font/google';
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({
-    subsets: ['latin']
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
     title: {
-        template: '%s | Flashcard App',
-        default: "Flashcard App"
+        template: "%s | Flashcard App",
+        default: "Flashcard App",
     },
     description: "Create your own flashcards and share them with friends!",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
     return (
-        <html
-            lang="en"
-            className={inter.className}
-        >
+        <html lang="en" className={inter.className} suppressHydrationWarning>
             <head>
                 <link
                     rel="icon"
@@ -28,7 +26,17 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
                     sizes="<generated>"
                 />
             </head>
-            <body>{children}</body>
+            <body>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                    enableColorScheme={false}
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
         </html>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
+import ModeToggle from "./components/mode-toggle";
 
 export default function Home() {
     return (
-        <header>
-            <nav className="sticky inset-s-0 top-0 w-full border-b bg-neutral-200 p-2">
-                <Link href="/" className="text-2xl font-semibold">
-                    Flashcard App
-                </Link>
-            </nav>
-        </header>
+        <>
+            <header>
+                <nav className="sticky inset-s-0 top-0 flex w-full items-center justify-between border-b bg-neutral-200 p-2 dark:bg-black">
+                    <Link
+                        href="/"
+                        className="text-xl font-semibold md:text-2xl dark:text-white"
+                    >
+                        Flashcard App
+                    </Link>
+                    <ModeToggle />
+                </nav>
+            </header>
+            <main>
+                
+            </main>
+        </>
     );
 }
