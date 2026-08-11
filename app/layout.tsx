52458ad04a10c85b6ca5edaf77d3924e9dd4d0d1
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-11 13:15:53 -0500

    Add Inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import {Inter} from 'next/font/google';
+
+const inter = Inter({
+    subsets: ['latin']
+});
 
 export const metadata: Metadata = {
     title: {
@@ -13,6 +18,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     return (
         <html
             lang="en"
+            className={inter.className}
         >
             <head>
                 <link
