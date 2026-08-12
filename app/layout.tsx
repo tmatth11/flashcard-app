2fd916c2989e4ebeef15d8effe21efda20879e5d
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-12 17:38:17 -0500

    Add Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,15 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import Link from "next/link";
+import ModeToggle from "@/app/components/mode-toggle";
+import {
+    ClerkProvider,
+    Show,
+    SignInButton,
+    SignUpButton,
+    UserButton,
+} from "@clerk/nextjs";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -27,15 +36,45 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
                 />
             </head>
             <body>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                    disableTransitionOnChange
-                    enableColorScheme={false}
-                >
-                    <div className="min-h-dvh">{children}</div>
-                </ThemeProvider>
+                <ClerkProvider>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                        enableColorScheme={false}
+                    >
+                        <header className="sticky inset-s-0 top-0 w-full border-b bg-neutral-200 p-4 dark:bg-black">
+                            <nav className="flex items-center justify-between">
+                                <Link
+                                    href="/"
+                                    className="text-2xl font-semibold dark:text-white"
+                                >
+                                    Flashcard App
+                                </Link>
+                                <div className="flex items-center gap-4">
+                                    <ModeToggle />
+                                    <Show when="signed-out">
+                                        <SignInButton>
+                                            <span className="cursor-pointer text-center dark:text-white">
+                                                Log In
+                                            </span>
+                                        </SignInButton>
+                                        <SignUpButton>
+                                            <button className="nav-button bg-blue-700">
+                                                Sign Up
+                                            </button>
+                                        </SignUpButton>
+                                    </Show>
+                                    <Show when="signed-in">
+                                        <UserButton />
+                                    </Show>
+                                </div>
+                            </nav>
+                        </header>
+                        <main className="min-h-dvh">{children}</main>
+                    </ThemeProvider>
+                </ClerkProvider>
             </body>
         </html>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,7 @@
-import Link from "next/link";
-import ModeToggle from "@/app/components/mode-toggle";
-
 export default function Home() {
     return (
         <>
-            <header className="sticky inset-s-0 top-0 w-full border-b bg-neutral-200 p-4 dark:bg-black">
-                <nav className="flex items-center justify-between">
-                    <Link
-                        href="/"
-                        className="text-xl font-semibold md:text-2xl dark:text-white"
-                    >
-                        Flashcard App
-                    </Link>
-                    <ModeToggle />
-                </nav>
-            </header>
-            <main>
-                
-            </main>
+            
         </>
     );
 }
