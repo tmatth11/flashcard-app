c9c14ffa0b65b00423f4e0026d091042dba3d0ae
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-16 18:45:36 -0500

    Start LoggedInLayout

---
+++ app/(logged_in)/layout.tsx
@@ -0,0 +1,12 @@
+export default function LoggedInLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <section>
+            
+            {children}
+        </section>
+    )
+}
