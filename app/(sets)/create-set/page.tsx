b62078d72579b7387a0ca69bf23b51c91b9deef4
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-20 18:46:28 -0500

    Add visibility toggle

--- app/(sets)/create-set/page.tsx
+++ app/(sets)/create-set/page.tsx
@@ -5,13 +5,25 @@ export default async function Page() {
 
     return (
         <div className="flex flex-col items-center p-2">
-            <div className="lg:w-3xl md:w-2xl">
+            <div className="md:w-2xl lg:w-3xl">
                 <div className="mt-5 flex flex-col items-center justify-between gap-2 md:flex-row md:gap-0">
-                    <h1 className="text-xl text-center font-semibold">
+                    <h1 className="text-center text-xl font-semibold">
                         Create a new flashcard set
                     </h1>
                     <button className="button bg-blue-500">Create</button>
                 </div>
+                <select
+                    className="mt-4 cursor-pointer"
+                    id="visiblity"
+                    name="visiblity"
+                >
+                    <option value="public" className="text-black">
+                        Public
+                    </option>
+                    <option value="private" className="text-black">
+                        Private
+                    </option>
+                </select>
             </div>
         </div>
     );
