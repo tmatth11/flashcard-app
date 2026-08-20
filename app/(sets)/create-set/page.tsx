07e2888e16b2c564f8354bb4c5a3717edfdbed9d
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-20 16:24:05 -0500

    Protect /create-set page

--- app/(sets)/create-set/page.tsx
+++ app/(sets)/create-set/page.tsx
@@ -1,4 +1,8 @@
-export default function Page() {
+import { auth } from "@clerk/nextjs/server";
+
+export default async function Page() {
+    await auth.protect();
+
     return (
         <p>/create-set works!</p>
     );
