e3665e8c251a87960867df098e59cfa15927248e
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-21 11:41:07 -0500

    Start card input styling

--- app/(sets)/create-set/page.tsx
+++ app/(sets)/create-set/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@clerk/nextjs/server";
+import { Move, Trash2 } from "lucide-react";
 
 export default async function Page() {
     await auth.protect();
@@ -24,6 +25,29 @@ export default async function Page() {
                         Private
                     </option>
                 </select>
+                <div className="mt-4 rounded-md bg-slate-700 p-2">
+                    <div className="flex items-center justify-between p-2 text-white">
+                        <span className="break-all">1</span>
+                        <div className="flex items-center gap-4">
+                            <button aria-label="Move card">
+                                <Move />
+                            </button>
+                            <button aria-label="Delete card">
+                                <Trash2 />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="mt-2 flex flex-col gap-4 md:flex-row justify-between">
+                        <div className="flex flex-col w-full">
+                            <textarea name="term" id="term" className="bg-slate-800 rounded-md p-1 text-white"></textarea>
+                            <label htmlFor="term" className="text-white">Term</label>
+                        </div>
+                        <div className="flex flex-col w-full">
+                            <textarea name="definition" id="definition" className="bg-slate-800 rounded-md p-1 text-white"></textarea>
+                            <label htmlFor="definition" className="text-white">Definition</label>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
