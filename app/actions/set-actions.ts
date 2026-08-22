cf20859a65a76472dd5f773d1000c06362775929
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-22 18:31:13 -0500

    Start createFlashcardSet action

--- app/actions/set-actions.ts
+++ app/actions/set-actions.ts
@@ -1,7 +1,11 @@
 'use server';
 
+import { auth } from '@clerk/nextjs/server';
 import { drizzle } from 'drizzle-orm/neon-http';
 import z from 'zod';
+import { flashcard, flashcardSet } from '../_db/schema';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const db = drizzle(process.env.DATABASE_URL!);
 
@@ -24,13 +28,78 @@ export type FlashcardSetState = {
         title?: string[],
         description?: string[];
         public?: string[];
-        cards?: {
-            term?: string[];
-            definition?: string[]
-        }[];
+        cards?: string[];
     };
 };
 
 export async function createFlashcardSet(prevState: FlashcardSetState, formData: FormData) {
+    const {userId} = await auth();
 
-}
+    if (!userId) {
+        return {
+            message: "Unauthorized. Please sign in to create flashcards sets.",
+            success: false,
+        };
+    }
+
+    const rawTitle = formData.get("title");
+    const rawDescription = formData.get("description")?.toString().trim() || undefined;
+    const rawPublic = formData.get("public");
+    const terms = formData.getAll("term") as string[];
+    const definitions = formData.getAll("definition") as string[];
+
+    const rawCards = terms.map((term, i) => ({
+        term: term || "",
+        definition: definitions[i] || ""
+    }));
+
+    const validatedFields = FlashcardSetSchema.safeParse({
+        title: rawTitle,
+        description: rawDescription || undefined,
+        public: rawPublic === "on",
+        cards: rawCards,
+    });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Missing or invalid fields. Please check your inputs.",
+            success: false,
+        };
+    }
+
+    const data = validatedFields.data;
+
+    try {
+        const [newSet] = await db
+            .insert(flashcardSet)
+            .values({
+                title: data.title,
+                description: data.description,
+                public: data.public,
+                userId: userId
+            })
+            .returning();
+
+        const cardsToInsert = data.cards.map((card, index) => ({
+            setId: newSet.id,
+            term: card.term,
+            definition: card.definition,
+            order: index,
+        }));
+
+        await db.insert(flashcard).values(cardsToInsert);
+    }
+    catch (error) {
+        console.error("Database Error:", error);
+        return {
+            message: "Database error: Failed to create flashcard set.",
+            success: false,
+        };
+    }
+
+    const redirectPath = "/my-sets";
+
+    revalidatePath(redirectPath);
+    redirect(redirectPath);
+};
