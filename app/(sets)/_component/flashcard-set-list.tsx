95b0349c84521c763e9a2ed028664e84355b4f8d
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-24 13:16:23 -0500

    Reveal number of terms

--- app/(sets)/_component/flashcard-set-list.tsx
+++ app/(sets)/_component/flashcard-set-list.tsx
@@ -5,19 +5,27 @@ export default async function FlashcardSetList(props: FlashcardSetProps) {
 
     return (
         <div className="mt-4 flex flex-col justify-center gap-4 md:w-xl lg:w-3xl">
-            {flashcardSets.map((flashcardSet) => (
-                <div
-                    key={flashcardSet.id}
-                    className="flex flex-col rounded-md bg-neutral-200 p-2 dark:bg-slate-700 dark:text-white"
-                >
-                    <span className="line-clamp-2 text-lg font-semibold break-all">
-                        {flashcardSet.title}
-                    </span>
-                    <span className="line-clamp-2 text-sm break-all">
-                        {flashcardSet.description}
-                    </span>
-                </div>
-            ))}
+            {flashcardSets.map((flashcardSet) => {
+                return (
+                    <div
+                        key={flashcardSet.id}
+                        className="flex flex-col rounded-md bg-neutral-200 p-2 dark:bg-slate-700 dark:text-white"
+                    >
+                        <div>
+                            <span className="line-clamp-2 text-sm break-all">
+                                {flashcardSet.termCount} Term
+                                {flashcardSet.termCount > 1 ? "s" : ""}
+                            </span>
+                        </div>
+                        <span className="line-clamp-2 text-lg font-semibold break-all">
+                            {flashcardSet.title}
+                        </span>
+                        <span className="line-clamp-2 text-sm break-all">
+                            {flashcardSet.description}
+                        </span>
+                    </div>
+                );
+            })}
         </div>
     );
 }
--- app/_lib/data.ts
+++ app/_lib/data.ts
@@ -1,9 +1,17 @@
 import { drizzle } from 'drizzle-orm/neon-http';
-import { flashcardSet } from '../_db/schema';
-import { eq } from "drizzle-orm";
+import { flashcard, flashcardSet } from '../_db/schema';
+import { count, eq, getTableColumns } from "drizzle-orm";
 
 const db = drizzle(process.env.DATABASE_URL!);
 
 export async function getAllFlashcardSets(filters: FlashcardSetProps) {
-    return db.select().from(flashcardSet).where(eq(flashcardSet.public, filters.isPublic));
+    return db
+    .select({
+        ...getTableColumns(flashcardSet),
+        termCount: count(flashcard.id)
+    })
+    .from(flashcardSet)
+    .leftJoin(flashcard, eq(flashcardSet.id, flashcard.setId))
+    .where(eq(flashcardSet.public, filters.isPublic))
+    .groupBy(flashcardSet.id);
 }
