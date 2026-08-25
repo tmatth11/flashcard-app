556cea7af1cb08b4d9ddd99928b5de323790a2f7
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-25 16:55:58 -0500

    Add Suspense to FlashcardSetList

--- app/(sets)/_components/flashcard-set-view.tsx
+++ app/(sets)/_components/flashcard-set-view.tsx
@@ -2,6 +2,7 @@ import { fetchFlashcardSetsPages } from "@/app/_lib/data";
 import FlashcardSetList from "./flashcard-set-list";
 import Pagination from "./pagination";
 import Searchbox from "./searchbox";
+import { Suspense } from "react";
 
 export default async function FlashcardSetView({
     placeholder,
@@ -20,15 +21,22 @@ export default async function FlashcardSetView({
         isPublic: isPublic,
         isPrivate: isPrivate,
         query: query,
-        currentPage: currentPage
+        currentPage: currentPage,
     };
 
     const totalPages = await fetchFlashcardSetsPages(filters);
 
     return (
-        <div className="flex flex-col justify-center w-3xs md:w-xl lg:w-3xl">
+        <div className="flex w-3xs flex-col justify-center md:w-xl lg:w-3xl">
             <Searchbox placeholder={placeholder} />
-            <FlashcardSetList isPublic={isPublic} isPrivate={isPrivate} query={query} currentPage={currentPage} />
+            <Suspense fallback={<p className="mt-4 text-center">Loading...</p>}>
+                <FlashcardSetList
+                    isPublic={isPublic}
+                    isPrivate={isPrivate}
+                    query={query}
+                    currentPage={currentPage}
+                />
+            </Suspense>
             <Pagination totalPages={totalPages} />
         </div>
     );
