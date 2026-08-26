1246859e8326d2a850a31651458226ab9897b75a
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-26 13:44:31 -0500

    Bring back /my-sets route

---
+++ app/(sets)/my-sets/page.tsx
@@ -0,0 +1,12 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+    const user = await currentUser();
+
+    if (!user || !user.username) {
+        redirect("/sign-in");
+    }
+
+    redirect(`/sets/${user.username}`);
+}
