997cbd331f551959f8ae0ce4bdb86b54b8677107
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-08-27 21:09:01 -0500

    Show message if no flashcard sets are present

--- app/(sets)/_components/flashcard-set-list.tsx
+++ app/(sets)/_components/flashcard-set-list.tsx
@@ -20,64 +20,65 @@ export default async function FlashcardSetList(props: FlashcardSetProps) {
 
     return (
         <div className="mt-4 flex flex-col justify-center gap-4">
-            {flashcardSets.map((flashcardSet) => {
-                return (
-                    <div
-                        key={flashcardSet.id}
-                        className="flex flex-col rounded-md bg-neutral-200 p-2 dark:bg-slate-700 dark:text-white"
-                    >
-                        <div className="line-clamp-2 text-sm break-all">
-                            <span>{flashcardSet.termCount} Term</span>
-                            <span>
-                                {flashcardSet.termCount > 1 ? "s" : ""}{" "}
-                            </span>
-                            {isUserPage ?? (
+            {(flashcardSets.length == 0 && <p className="text-center">No flashcard sets were found.</p>) ||
+                flashcardSets.map((flashcardSet) => {
+                    return (
+                        <div
+                            key={flashcardSet.id}
+                            className="flex flex-col rounded-md bg-neutral-200 p-2 dark:bg-slate-700 dark:text-white"
+                        >
+                            <div className="line-clamp-2 text-sm break-all">
+                                <span>{flashcardSet.termCount} Term</span>
                                 <span>
-                                    <span>| </span>
-                                    <Image
-                                        width="20"
-                                        height="20"
-                                        src={flashcardSet.imageUrl}
-                                        alt={flashcardSet.username}
-                                        className="inline-block rounded-full"
-                                    />
-                                    <span> </span>
-                                    <Link
-                                        href={`/sets/${flashcardSet.username}`}
-                                        className="hover:underline"
-                                    >
-                                        {flashcardSet.username}
-                                    </Link>
+                                    {flashcardSet.termCount > 1 ? "s" : ""}{" "}
                                 </span>
+                                {isUserPage ?? (
+                                    <span>
+                                        <span>| </span>
+                                        <Image
+                                            width="20"
+                                            height="20"
+                                            src={flashcardSet.imageUrl}
+                                            alt={flashcardSet.username}
+                                            className="inline-block rounded-full"
+                                        />
+                                        <span> </span>
+                                        <Link
+                                            href={`/sets/${flashcardSet.username}`}
+                                            className="hover:underline"
+                                        >
+                                            {flashcardSet.username}
+                                        </Link>
+                                    </span>
+                                )}
+                            </div>
+
+                            <span className="line-clamp-2 text-lg font-semibold break-all">
+                                {flashcardSet.title}{" "}
+                                {isOwner && !flashcardSet.public && (
+                                    <span className="inline-block">
+                                        <Lock size={16} />
+                                    </span>
+                                )}
+                            </span>
+                            <span className="line-clamp-2 text-sm break-all">
+                                {flashcardSet.description}
+                            </span>
+                            {isOwner && (
+                                <div className="flex justify-end gap-2">
+                                    <button className="button bg-yellow-600 hover:bg-yellow-500">
+                                        Edit
+                                    </button>
+                                    <DeleteFlashcardSetButton
+                                        id={flashcardSet.id}
+                                        username={flashcardSet.username}
+                                        filters={filters}
+                                    />
+                                </div>
                             )}
                         </div>
-
-                        <span className="line-clamp-2 text-lg font-semibold break-all">
-                            {flashcardSet.title}{" "}
-                            {isOwner && !flashcardSet.public && (
-                                <span className="inline-block">
-                                    <Lock size={16} />
-                                </span>
-                            )}
-                        </span>
-                        <span className="line-clamp-2 text-sm break-all">
-                            {flashcardSet.description}
-                        </span>
-                        {isOwner && (
-                            <div className="flex justify-end gap-2">
-                                <button className="button bg-yellow-600 hover:bg-yellow-500">
-                                    Edit
-                                </button>
-                                <DeleteFlashcardSetButton
-                                    id={flashcardSet.id}
-                                    username={flashcardSet.username}
-                                    filters={filters}
-                                />
-                            </div>
-                        )}
-                    </div>
-                );
-            })}
+                    );
+                })}
         </div>
     );
 }
