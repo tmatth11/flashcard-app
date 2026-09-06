af89660f7ab37f25f51257b7f93e15f055bf8224
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-09-06 17:44:59 -0500

    Clean up styles in navbar.tsx

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -90,7 +90,7 @@ export default function Navbar() {
                         </Link>
                         <Link
                             href="/sign-up"
-                            className="button bg-blue-700 text-center"
+                            className="button blue-button"
                             onClick={() => {
                                 setLinksDisplay("hidden");
                                 window.scrollTo(0, 0);
