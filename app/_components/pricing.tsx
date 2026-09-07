c731d1f553b06dfce48627423086e4cd8a8c1c7a
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-09-07 13:28:27 -0500

    Create custom container color

--- app/_components/pricing.tsx
+++ app/_components/pricing.tsx
@@ -5,7 +5,7 @@ export default function Pricing() {
     return (
         <section className="home-section">
             <h1 className="text-2xl font-semibold lg:text-3xl">Pricing</h1>
-            <div className="flex h-90 w-70 flex-col items-center justify-center gap-2 rounded-md bg-neutral-300 p-2 dark:bg-slate-700">
+            <div className="bg-container-primary flex h-90 w-70 flex-col items-center justify-center gap-2 rounded-md p-2">
                 <h2 className="text-lg font-semibold">Flashcard App</h2>
                 <p>
                     <span className="text-3xl font-bold">$0.00</span> / month
