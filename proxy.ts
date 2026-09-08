31863d79419e4651f1c0ae68756a3a6f8f97087e
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-09-07 20:48:27 -0500

    Enable clerk frontendApiProxy

--- proxy.ts
+++ proxy.ts
@@ -1,14 +1,15 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+    frontendApiProxy: {
+        enabled: true,
+    },
+});
 
 export const config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-        // Always run for Clerk-specific frontend API routes
-        '/__clerk/(.*)',
+        "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+        "/(api|trpc)(.*)",
+        "/__clerk/(.*)",
     ],
 };
