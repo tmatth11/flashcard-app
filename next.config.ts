682623a75abe0e20583537b4df6dbbdd42b6223a
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-09-07 20:35:04 -0500

    Revert "Update next.config.ts"
    This reverts commit 58a2bb2b3210ebc21a541771192285a1c53d46b3.

--- next.config.ts
+++ next.config.ts
@@ -14,14 +14,6 @@ const nextConfig: NextConfig = {
     experimental: {
         authInterrupts: true,
     },
-    async rewrites() {
-        return [
-            {
-                source: "/__clerk/:path*",
-                destination: "https://clerk.flashcard-app-eight-theta.vercel.app/__clerk/:path*",
-            },
-        ];
-    },
 };
 
 export default nextConfig;
